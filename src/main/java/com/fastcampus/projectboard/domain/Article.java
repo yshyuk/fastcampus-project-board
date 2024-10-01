04020dc382067055e02e1d5902c8821991d9b4fa
yshyuk <dbsrbtkd94@gmail.com>
2024-10-01 19:42:59 +0900

    #8 도메인 클래스 설계 (게시글, 댓글)

---
+++ src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -0,0 +1,26 @@
+package com.fastcampus.projectboard.domain;
+
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Article {
+
+    private Long id;    // 식별자
+
+    private String title;   // 제목
+
+    private String content; // 본문
+
+    private String hashtag; // 해시태그
+
+    private LocalDateTime createdAt; // 생성일시
+
+    private String createdBy; // 생성자
+
+    private LocalDateTime modifiedAt; // 수정일시
+
+    private String modifiedBy; // 수정자
+}
